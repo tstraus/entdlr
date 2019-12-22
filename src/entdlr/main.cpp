86c0f33b457a4d4be7855d7d82385230f2a45f59
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2019-12-22 15:44:06 -0500

    samples

--- src/entdlr/main.cpp
+++ src/entdlr/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
+//#include "antlr4-runtime/antlr4-runtime.h"
+//#include "entdlrLexer.h"
+//#include "entdlrParser.h"
+//#include "entdlrVisitor.h"
+
 using std::cout; using std::endl;
 
 int main(int argc, char** argv)
 {
     cout << "entdlr" << endl;
+
+    /*std::ifstream file;
+    file.open("../example.proto");
+
+    antlr4::ANTLRInputStream input(file);
+    entdlrLexer lexer(&input);
+    antlr4::CommonTokenStream tokens(&lexer);
+    entdlrParser parser(&tokens);*/
 
     return 0;
 }
