a5f5cfd6f02144a7b6050a493fcc8956b2915f5c
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2019-12-31 10:28:20 -0500

    project reorg

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 pushd ./src/entdlr/grammar &> /dev/null
-antlr4 -visitor -o ../generated -Dlanguage=Cpp FlatBuffers.g4 
+antlr4 -visitor -o ./generated -Dlanguage=Cpp FlatBuffers.g4 
---
+++ src/entdlr/context/context.h
@@ -0,0 +1,6 @@
+#ifndef __ENTDLR_CONTEXT_H__
+#define __ENTDLR_CONTEXT_H__
+
+
+
+#endif // __ENTDLR_CONTEXT_H__
