3250deb379cf24d08a095a7eefa47b5c2ed17781
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2021-06-18 12:21:06 -0400

    add include dir to cmake command

--- src/entdlr/entdlr.cmake
+++ src/entdlr/entdlr.cmake
@@ -7,9 +7,16 @@ macro (ENTDLR_TARGET DefinitionFile TemplateFile OutputFile)
     get_filename_component (Filename ${DefinitionFile} NAME)
     string (REGEX REPLACE "\(.*\).fbs" "\\1" Name ${Filename})
 
+    # parse option arguments
+    set (optional_args ${ARGN})
+    set (includeOption "-i=")
+    if (NOT "${optional_args}" STREQUAL "")
+        set (includeOption "-i=${optional_args}")
+    endif ()
+
     add_custom_command (
         OUTPUT ${OutputFile}
-        COMMAND ${ENTDLR_EXECUTABLE} "-t=${TemplateFile}" "-f=${DefinitionFile}" "-o=${OutputFile}"
+        COMMAND ${ENTDLR_EXECUTABLE} "-t=${TemplateFile}" "-f=${DefinitionFile}" "-o=${OutputFile}" "${includeOption}"
         VERBATIM
         DEPENDS ${TemplateFile} ${DefinitionFile} ${ENTDLR_EXECUTABLE}
         COMMENT "[ENTDLR][${Name}]"
