47191f1819f8306122909c363bcd553ac40ebf0d
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2021-08-17 21:49:42 -0400

    update cmake

--- src/entdlr/entdlr.cmake
+++ src/entdlr/entdlr.cmake
@@ -1,11 +1,14 @@
 set (ENTDLR_ROOT "${CMAKE_CURRENT_LIST_DIR}/../")
 set (ENTDLR_EXECUTABLE "${ENTDLR_ROOT}/bin/entdlr")
 
-# Make ENTDLR parser a file
+# Make ENTDLR parse a file
+# Setting the variable "appendTemplateFilename" to true will append the template name to the output variable name
 macro (ENTDLR_TARGET DefinitionFile TemplateFile OutputFile)
     # get a project name from it
     get_filename_component (Filename ${DefinitionFile} NAME)
+    get_filename_component (Template_Filename ${TemplateFile} NAME)
     string (REGEX REPLACE "\(.*\).fbs" "\\1" Name ${Filename})
+    string (REGEX REPLACE "\(.*\).tmpl" "\\1" Template_Filename ${Template_Filename})
 
     # parse option arguments
     set (optional_args ${ARGN})
@@ -23,5 +26,11 @@ macro (ENTDLR_TARGET DefinitionFile TemplateFile OutputFile)
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
     )
 
-    set (ENTDLR_${Name}_OUTPUT ${OutputFile})
+    # Applying the template filename is useful for when the same definition file is used with multiple template files
+    #  without appending the template file name the previously generated file would be overwritten
+    if (appendTemplateFilename)
+        set (ENTDLR_${Name}_${Template_Filename}_OUTPUT ${OutputFile})
+    else()
+        set (ENTDLR_${Name}_OUTPUT ${OutputFile})
+    endif()
 endmacro ()
